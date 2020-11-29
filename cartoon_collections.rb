d166cf463de63f546566e8296b2f476a08eeac72
Alexander S <alx.sherwin@gmail.com>
2020-11-29 21:36:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
 planeteer_calls.find {|e| e == valid_calls}  
-planeteer_calls.include?(valid_calls)
 
 
 end
